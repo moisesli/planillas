c1590d3f8d2ae119d1223aafc229c490382cff8e
moises <elnaufrago2009@gmail.com>
2019-12-10 19:48:18 -0500

    planillas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueProgressBar from 'vue-progressbar'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
@@ -13,6 +14,22 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 library.add(faDollarSign,faUser,faHome,faLockOpen,faBook, faAddressCard, faCalendar,faCog)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options)
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usuario: false,
+    plantilla: false
   },
   mutations: {
+    generaPlantilla(state, plantilla) {
+      state.plantilla = plantilla;
+    },
+    logearUsuario(state){
+      state.usuario = true;
+    },
+    logoutUsuario(state){
+      state.usuario = false;
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
